be797b19c4fefb118662169370285bc965d945e2
Zulko <valentin.zulkower@gmail.com>
2014-01-17 12:51:44 +0100

    changed version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name='picnic',
-      version='0.0.0.3',
+      version='0.0.0.4',
       author='Zulko 2014',
      description='Python packages creation made easy',
      long_description=open('README.rst').read(),
